8719ee29d4bbcdf36e090dea3b2f2567a055118e
houndhollis <houndhollis9797@gmail.com>
2025-04-21 22:23:20 +0900

    feat: useProdcut 로직 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,25 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [product, setProduct] = useState<Product[]>(() => initialProducts);
+
+  const handleUpdateProduct = (updateProduct: Product) => {
+    setProduct((prev) => {
+      return prev.map((product) =>
+        product.id === updateProduct.id ? { ...updateProduct } : { ...product }
+      );
+    });
+  };
+
+  const handleAddProduct = (newProduct: Product) => {
+    setProduct((prev) => {
+      return [...prev, newProduct];
+    });
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products: product,
+    updateProduct: handleUpdateProduct,
+    addProduct: handleAddProduct,
   };
 };
